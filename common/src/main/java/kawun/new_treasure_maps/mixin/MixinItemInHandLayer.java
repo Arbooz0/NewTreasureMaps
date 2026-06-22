414a6ca387529c192dc44f3db268853344a457b8
Kirill <a8kirill@gmail.com>
2026-06-22 08:37:55 +0300

    SpecialModelRenderer map delete

--- common/src/main/java/kawun/new_treasure_maps/mixin/MixinItemInHandLayer.java
+++ common/src/main/java/kawun/new_treasure_maps/mixin/MixinItemInHandLayer.java
@@ -1,26 +1,12 @@
 package kawun.new_treasure_maps.mixin;
 
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
-import kawun.new_treasure_maps.Constants;
-import kawun.new_treasure_maps.client.render.MapRenderer;
 import net.minecraft.client.model.ArmedModel;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.item.ItemStackRenderState;
-import net.minecraft.world.entity.HumanoidArm;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
-import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(ItemInHandLayer.class)
--- neoforge/src/main/java/kawun/new_treasure_maps/NewTreasureMapsNeoforge.java
+++ neoforge/src/main/java/kawun/new_treasure_maps/NewTreasureMapsNeoforge.java
@@ -1,23 +1,15 @@
 package kawun.new_treasure_maps;
 
 
-import kawun.new_treasure_maps.client.render.MapRenderer;
 import kawun.new_treasure_maps.commands.CommandRegister;
 import kawun.new_treasure_maps.items.Items;
-import kawun.new_treasure_maps.utils.Utils;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterSpecialModelRendererEvent;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.RegisterEvent;
 
 @Mod(Constants.MOD_ID)
@@ -41,11 +33,6 @@ public class NewTreasureMapsNeoforge {
         if (event.getRegistryKey().equals(Registries.ITEM)) {
             Items.register((id, item) -> event.register(Registries.ITEM, id, () -> item));
         }
-    }
-
-    @SubscribeEvent
-    public static void registerItemRenderers(RegisterSpecialModelRendererEvent event) {
-        //MapRenderer.regiter((i, c) -> event.register(i, c));
     }
 
 
